8661e0300e07eaa1f9b324896ead214c22162866
Anuar Jimenez <heyitsanuar@gmail.com>
2018-11-26 21:19:05 -0600

    Added few comments for explaining Fact-Slider's Javascript file.

--- app/assets/js/modules/Fact.js
+++ app/assets/js/modules/Fact.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
 class Fact {
+    
     constructor(){
         this.facts = $(".fact");
         this.backBtns = $(".fa-chevron-left:not(fact__point--disabled)");
@@ -14,11 +15,13 @@ class Fact {
         this.nextBtns.click(this.nextFact);
     }
 
+    //The slider will change to the next sibling fact
     nextFact(){
         $(this).parents(".fact").addClass("fact--hidden");
         $(this).parents(".fact").next().removeClass("fact--hidden");    
     }
 
+    //The slider will change to the previous sibling fact
     previousFact(){
         $(this).parents(".fact").addClass("fact--hidden");
         $(this).parents(".fact").prev().removeClass("fact--hidden");       
